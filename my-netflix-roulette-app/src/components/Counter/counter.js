af6e2591ada24d6d027ccf0deae861d912c77110
mamtaraovk68 <mamtarao9395@gmail.com>
2024-02-26 16:59:02 +0530

    fix indents

--- my-netflix-roulette-app/src/components/Counter/counter.js
+++ my-netflix-roulette-app/src/components/Counter/counter.js
@@ -1,44 +1,52 @@
-import React from 'react'
-import './counter.css'
+import React from "react";
+import "./counter.css";
 
 // CounterApp React class level component
-class CounterApp extends React.Component{
-
+class CounterApp extends React.Component {
   //Constructor to make use of component state
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      value: parseInt(props.initialValue, 10) || 0
-    }
+      value: parseInt(props.initialValue, 10) || 0,
+    };
 
     this.incrementCounter = this.incrementCounter.bind(this);
     this.decrementCounter = this.decrementCounter.bind(this);
   }
 
   // function to increase the counter value from 1
-  incrementCounter(){
+  incrementCounter() {
     this.setState({
-        value: this.state.value+1
-      })
+      value: this.state.value + 1,
+    });
   }
 
   // function to decrease the counter value from 1
-  decrementCounter(){
+  decrementCounter() {
     this.setState({
-        value: this.state.value-1
-    })
+      value: this.state.value - 1,
+    });
   }
 
   // Main render code for the CounterApp component. Re-renders when the state is changed
   render() {
-    return React.createElement('div',{ className: 'counter-container' },
-    React.createElement('p', null, 'Counter Value: ' + this.state.value),
-      React.createElement('button', { onClick: this.incrementCounter, className: 'incrementButton' }, 'Increment'),
-      React.createElement('span', { style: { margin: '0 10px' } }), // Adding a span for spacing
-      React.createElement('button', { onClick: this.decrementCounter, className: 'decrementButton' }, 'Decrement')
+    return React.createElement(
+      "div",
+      { className: "counter-container" },
+      React.createElement("p", null, "Counter Value: " + this.state.value),
+      React.createElement(
+        "button",
+        { onClick: this.incrementCounter, className: "incrementButton" },
+        "Increment"
+      ),
+      React.createElement("span", { style: { margin: "0 10px" } }), // Adding a span for spacing
+      React.createElement(
+        "button",
+        { onClick: this.decrementCounter, className: "decrementButton" },
+        "Decrement"
+      )
     );
   }
 }
-
 
 export default CounterApp;
--- my-netflix-roulette-app/src/components/SearchForm/search.js
+++ my-netflix-roulette-app/src/components/SearchForm/search.js
@@ -1,45 +1,45 @@
-import React, { useEffect } from 'react';
-import {useState} from 'react';
-import './search.css';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import "./search.css";
 
-function SearchApp({ query:initialQuery = '', onSearch }) {
-   const [query, setSearchQuery] = useState(initialQuery)
+function SearchApp({ query: initialQuery = "", onSearch }) {
+  const [query, setSearchQuery] = useState(initialQuery);
 
-   useEffect( () => {
+  useEffect(() => {
     setSearchQuery(initialQuery);
-   }, [initialQuery]);
+  }, [initialQuery]);
 
-    const handleSearch = () => {
-        onSearch(query);
+  const handleSearch = () => {
+    onSearch(query);
+  };
+
+  const handleInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
     }
-    
-    const handleInputChange = (event) => {
-        setSearchQuery(event.target.value);
-      };
+  };
 
-      const handleKeyPress = (e) => {
-        if (e.key === 'Enter') {
-          handleSearch();
-        }
-      };
-    
-    return ( 
-    <div className='search-bar-container'>
+  return (
+    <div className="search-bar-container">
       <p className="header-element">Find your movie</p>
       <input
-        className='search-input'
-        type="text" 
-        placeholder="Search..." 
+        className="search-input"
+        type="text"
+        placeholder="Search..."
         onKeyDown={handleKeyPress}
         onChange={handleInputChange}
-        value={query} />
-      <span style={{ margin: '0 10px' }}></span>
-      <button className='search-button' onClick={handleSearch}>Search</button>
+        value={query}
+      />
+      <span style={{ margin: "0 10px" }}></span>
+      <button className="search-button" onClick={handleSearch}>
+        Search
+      </button>
     </div>
-    );
-
-
+  );
 }
 
-
-export default SearchApp
+export default SearchApp;
